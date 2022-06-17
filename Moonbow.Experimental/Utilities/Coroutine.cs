b7a1a90009182bc9491a37d1d3886b2e2f5bf126
nyawoi <106277673+nyawoi@users.noreply.github.com>
2022-06-17 10:09:33 -0500

    Basic coroutine system added

---
+++ Moonbow.Experimental/Utilities/Coroutine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Plukit.Base;
+
+namespace AetharNet.Moonbow.Experimental.Utilities
+{
+    public static class Coroutine
+    {
+        internal static readonly LinkedList<IEnumerator<Timestep>> Coroutines = new();
+        
+        public static void StartCoroutine(IEnumerable<Timestep> coroutine)
+        {
+            Coroutines.AddLast(coroutine.GetEnumerator());
+        }
+        
+        public static void StartCoroutine(IEnumerator<Timestep> coroutine)
+        {
+            Coroutines.AddLast(coroutine);
+        }
+    }
+}
