94421e4a95e846ea06cfb5018a484df5d9e6559d
nyawoi <106277673+nyawoi@users.noreply.github.com>
2022-06-17 10:57:50 -0500

    Added utility methods

--- Moonbow.Experimental/Utilities/Coroutine.cs
+++ Moonbow.Experimental/Utilities/Coroutine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Plukit.Base;
 
 namespace AetharNet.Moonbow.Experimental.Utilities
@@ -15,6 +16,46 @@ namespace AetharNet.Moonbow.Experimental.Utilities
         public static void StartCoroutine(IEnumerator<Timestep> coroutine)
         {
             Coroutines.AddLast(coroutine);
+        }
+
+        public static void AwaitCondition(Func<bool> condition, Action callback)
+        {
+            StartCoroutine(AwaitConditionCoroutine(condition, callback));
+        }
+
+        public static void AwaitTimedCondition(Timestep expiration, Func<bool> condition, Action<bool> callback)
+        {
+            StartCoroutine(AwaitTimedConditionCoroutine(expiration, condition, callback));
+        }
+
+        private static IEnumerable<Timestep> AwaitConditionCoroutine(Func<bool> condition, Action callback)
+        {
+            while (true)
+            {
+                if (condition())
+                {
+                    callback();
+                    yield break;
+                }
+
+                yield return GameUtilities.Universe.Step + 1;
+            }
+        }
+
+        private static IEnumerable<Timestep> AwaitTimedConditionCoroutine(Timestep expiration, Func<bool> condition, Action<bool> callback)
+        {
+            while (true)
+            {
+                var step = GameUtilities.Universe.Step;
+
+                if (condition() || step > expiration)
+                {
+                    callback(step > expiration);
+                    yield break;
+                }
+
+                yield return step + 1;
+            }
         }
     }
 }
